265ee351bb10782df11ea7279590e3272ead5417
obewas <obewas1202@gmail.com>
2021-04-06 01:16:18 +0300

    added cost list

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('create/', views.CarCreateView.as_view(), name='create'),
     path('<pk>/update', views.CarUpdateView.as_view(), name='update'),
     path('car/<int:pk>/', views.CarDetailView.as_view(), name='car_detail'),
-    path('delete/<pk>/', views.DeleteView.as_view(), name='delete'),
+    path('<pk>/delete/', views.CarDeleteView.as_view(), name='delete'),
     path('total/' , views.total_cost, name='total'),
+    path('cost/', views.CostListView.as_view(), name='cost'),
 ]
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,43 +1,47 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Car
+from .models import Car, Cost
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404
 from django.views import View
 from .forms import CostCreationForm
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 # Create your views here.
-class CarListView(ListView):
+class CarListView(LoginRequiredMixin ,ListView):
     template_name = 'list.html'
     model = Car
     fields = '__all__'
 
-class CarCreateView(CreateView):
+class CarCreateView(LoginRequiredMixin ,CreateView):
     template_name = 'create_car.html'
     model = Car
     fields = '__all__'
     success_url = '/'
 
 
-class CarUpdateView(UpdateView):
+class CarUpdateView(LoginRequiredMixin ,UpdateView):
     template_name = 'update_car.html'
     model = Car
     fields = '__all__'
     success_url = '/'
-class CarDeleteView(DeleteView):
+class CarDeleteView(LoginRequiredMixin ,DeleteView):
     template_name = 'delete_car.html'
     model = Car
     fields = '__all__'
-    success_url ="/"
+    success_url = "/"
+    
+  
 
-class CarDetailView(DetailView):
+class CarDetailView(LoginRequiredMixin ,DetailView):
     template_name = 'car_detail.html'
     model = Car
     fields = '__all__'
 
 
-
+@login_required
 def total_cost(request):
    
     if request.method == "POST":
@@ -54,10 +58,13 @@ def total_cost(request):
             agency = form.cleaned_data["agency"]
     
             total = customs + cif_cost + delivery_order + radiation + ntsa_sticker + insurance + cfs_chgs + agency
-            
+            form.save()
             return render(request, "total_cost.html", {"form": form, "total": total})
     else:
         form = CostCreationForm()
     return render(request, "total_cost.html", {"form": form})
    
-       
+class CostListView(ListView):
+    template_name = 'cost_list.html'
+    model = Cost
+    fields = '__all__'
